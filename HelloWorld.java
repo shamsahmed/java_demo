18b3be10c08c55b85fba1ca9abddd9dec1f3d951
Shams <shams.ahmed@gmail.com>
2017-02-07 19:23:53 +0300

    introduced a loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,15 @@ class HelloWorld
 {
   public static void main(String[] args) {
     System.out.println("Welcome to GIT");
+    System.out.println("Welcome to GIT2");
+    System.out.println("Welcome to GIT3");
+    System.out.println("Welcome to GIT4");
+
+    for (int i = 10; i<10; i++ ) {
+      System.out.println("priting in loop to GIT4");
+
+
+    }
+
 }
 }
